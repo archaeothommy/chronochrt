a623e3f2f33871b60727b33c2abeb7fddb629a9a
roset <thomas.rose@daad-adlumni.de>
2019-09-20 23:36:37 +0300

    plot function completely working, text labels implemented

--- R/chronochrt_functions.R
+++ R/chronochrt_functions.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 library(readxl)
-library(imager)
+library(imager) # oder ggimage for plotting?
 # example/playground
 xy <- add_chron(xy,
                 c("A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "B", "C", "B", "B", "C", "C", "C", "C", "A", "A", "A"),
@@ -12,7 +12,14 @@ xy <- add_chron(xy,
                 new_table = TRUE) %>%
     add_chron(., c("D"), c("Test 1", "Test 2", "Test 3", "Test 3a", "Test 3b", "Test 4"), c(100, 200, 300, 300, 400, 500), c(200, 300, 500, 400, 500, 600), c(1, 1, 1, 2, 2, 1))
 
-plot_chronochrt(xy, years_major = 250)
+labels <- add_text_label(labels,
+                         c("A", "B"),
+                         c(-900, 0),
+                         c("LABEL1", "LABEL2"),
+                         add = FALSE) %>%
+  add_text_label(., c("C", "A"), c(-500, -1000), c("#", "LABEL4"))
+
+plot_chronochrt(xy, year = "Jahr", labels_text = labels, labels_x = 0.8, years_major = 250)
 
 # Make new chronological unit ---------------------------------------------
 
@@ -21,6 +28,7 @@ plot_chronochrt(xy, years_major = 250)
   # works only with the given naming scheme, for other ones use regular dplyr-functions (add_row)
 
   # to implement: telling error messages
+  # could probably be enhanced with missing-function to avoid input of data argument "new_table" analogous to plot-switches
 
 add_chron <- function(data, region, unit, start, end, level = 1, add = FALSE, new_table = FALSE, ...)
   {
@@ -34,7 +42,9 @@ add_chron <- function(data, region, unit, start, end, level = 1, add = FALSE, ne
 
   # makes a tibble out of a file, the relevant columns must be identified and will be renamed
 
-  # implementation needed: guessing of format from file name and choosing of right import method to get more flexibility
+  # implementation needed:
+    #guessing of format from file name and choosing of right import method to get more flexibility
+    # check if file exists with file.exists(...)
 
 import_chron <- function(path, region, unit, start, end, level, add = FALSE, ...)
   {
@@ -49,14 +59,15 @@ import_chron <- function(path, region, unit, start, end, level, add = FALSE, ...
   # add text labels
 
     # works analogous to add_chron
+    # could probably enhanced with missing-function to avoid input of data argument "new_table" analogous to plot-switches
 
-  add_label_text <- function (region, year, annotation, ...)
-  {
-    #if (!is.na(check_format())) {stop(check_format())}
-    labels <- tibble(region, year, annotation, ...)
-
-    labels
-  }
+add_text_label <- function(data, region, year, annotation, add = TRUE, ...)
+{
+  #if (!is.na(check_format())) {stop(check_format())}
+  if (add == FALSE) {data <- tibble(region, year, annotation, ...)}
+  if (add == TRUE) {data <- add_row(data, region, year, annotation, ...)}
+  data
+}
 
   # add image labels
 
@@ -80,12 +91,10 @@ import_chron <- function(path, region, unit, start, end, level, add = FALSE, ...
 
 # Plot chart --------------------------------------------------------------------
 
-  # to implement: differentiation between e. g. Test 2b and Test 2a1 (last subunits and subunits with missing column)
-  # current: all last subunits affected
+  # in plot: switches/input for: labels, breaks auf y-Achse
+  # labels are right-aligned to the given x co-ordinate to avoid running out of bounds
 
-  # in plot: switches/input for: labels, breaks auf y-Achse,
-
-plot_chronochrt <- function(data, year, labels_text, years_major = 100, breaks_minor = 1, labels_image)
+plot_chronochrt <- function(data, year = "years", labels_text = NULL, labels_image = NA, labels_x =0.9 , years_major = 100, breaks_minor = 1)
   {
   data <- data %>% # calculation of geometry
     group_by(region, add) %>%
@@ -93,32 +102,37 @@ plot_chronochrt <- function(data, year, labels_text, years_major = 100, breaks_m
     mutate(col_tot = case_when(level > subchron ~ level,
                                level == subchron ~ level +1,
                                level < subchron ~ level + subchron)) %>%
-    mutate(x_center = (level-0.5)/col_tot, # column_pos lowered by one to keep counting of position starting at 1, i. e. coherent to the natural counting habit and coherent mit counting for column_tot
+    mutate(x_center = (level-0.5)/col_tot,
            x_width = 1/col_tot,
            y_center = (start+end)/2,
            y_width = end - start) %>%
-    mutate_at(., .vars = c(x_center, x_width), .funs = width_corr(x_center, x_width)) %>%
-   # mutate(x_width = if_else(level > subchron & subchron != 0, true = 1/col_tot + 1/(2*(col_tot+1)), false = x_width),
-   #       x_center = if_else(level > subchron & subchron != 0, true = (level-0.5)/col_tot - 1/(4*(col_tot+1)), false = x_center)) %>%
+    mutate(x_center_corr = center_corr(x_center, x_width),
+           x_width_corr = width_corr(x_center, x_width)) %>%
+    mutate(x_center = x_center_corr,
+           x_width = x_width_corr) %>%
+    select(-x_center_corr, -x_width_corr) %>%
     mutate(x_center = replace(x_center, add == TRUE, x_center + 1))
 
   plot <- ggplot(data) + # plot
     geom_tile(aes(x = x_center, width = x_width, y = y_center, height = y_width), fill = "white", color = "black", linetype = "solid") +
     geom_text(aes(x = x_center, y = y_center, label = unit)) +
+    if(!missing(labels_text)) {
+      geom_text(data = labels_text, aes(y=year, x = labels_x, label = annotation, hjust = 1), na.rm = TRUE, size = 2)
+    }
+  # if(!missing(labels_image)) {
+  #   geom_text(data = labels_text, aes(y=year, x = labels_x, label = annotation, hjust = 1), na.rm = TRUE, size = 2)
+  # }
+  plot <- plot +
     scale_x_continuous(name = "", breaks = NULL, minor_breaks = NULL, expand = c(0,0)) +
-    scale_y_continuous(name = "", breaks = round(seq(min(data$start), max(data$end), by = years_major),1)) +
+    scale_y_continuous(name = year, breaks = round(seq(min(data$start), max(data$end), by = years_major),1)) +
     facet_grid(cols = vars(region), scales = "free_x", space = "free_x") +
     theme_chronochrt()
-
-   # geom_text(data = labels_text, aes(y=year, x = ###, label = annotation, hjust = 0, vjust = 0.5), na.rm = TRUE, size = 2)
-
 
   plot
 }
 
 # ChronochRt theme --------------------------------------------------------
-
-# based on theme_grey, basic theme in ggplot2
+    # based on theme_grey, basic theme in ggplot2
 theme_chronochrt <- function (base_size = 11, base_family = "", base_line_size = base_size / 22, base_rect_size = base_size / 22)
   {
     theme_grey(
@@ -156,7 +170,7 @@ check_format <- function()
 }
 
 
-# Helper function ---------------------------------------------------------
+# Helper functions ---------------------------------------------------------
 
 subchron_count <- function(left, right) # counts subchrons
   {
@@ -172,24 +186,36 @@ subchron_count <- function(left, right) # counts subchrons
       }
     }
     data
-  }
+}
 
-
-width_corr <- function(x_center, x_width) # corrects width if there are missing subunits in other subchrons
+center_corr <- function(center, width) # corrects center if there are missing subunits in other subchrons
   {
-    data <- rep_len(0, length(x_center))
-
-    for(i in 1:length(x_center))
+  for(i in 1:length(center))
     {
-      for(j in 1:length(x_center))
+      for(j in 1:length(center))
       {
-        k <- x_center[i] + (x_width[i]/2) - x_center[j] + (x_width[j]/2),
+        k <- center[j] - (width[j]/2) - center[i] - (width[i]/2);
 
-        if (x_center[i] < x_center[j] & k != 0 & k < x_width[i]){
-          x_center[j] <- x_center[j] - k/2;
-          x_width[j] <-  x_width[j] + k
-          }
+        if (center[i] <= center[j] & k > 0 & k < 1 & k <= width[i]){
+            center[j] <- center[j] - k/2;
+        }
       }
     }
-    data
+  center
+}
+
+width_corr <- function(center, width) # corrects width if there are missing subunits in other subchrons
+{
+  for(i in 1:length(center))
+  {
+    for(j in 1:length(center))
+    {
+      k <- center[j] - (width[j]/2) - center[i] - (width[i]/2);
+
+      if (center[i] <= center[j] & k > 0 & k < 1 & k <= width[i]){
+          width[j] <-  width[j] + k
+      }
+    }
   }
+  width
+}
