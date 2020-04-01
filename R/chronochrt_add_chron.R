7483edb3bb43228f95739bb49a9301f2a990035a
Thomas Rose <thomas.rose@daad-alumni.de>
2020-04-01 17:10:12 +0300

    some minor fixes

--- R/chronochrt_add_chron.R
+++ R/chronochrt_add_chron.R
@@ -6,7 +6,7 @@
 #' \code{start} and \code{end} of neighbouring chronological units must be fit to get a good plotting results, as well as the start date of e.g. the top chronological unit and its respective oldest sub-units. Dates in BCE must provided as negative data. Currently, only years can be handled (i.e. 2020 but not 20.20.2020).
 #' Is \code{start} and \code{end} dates are not certain or the change between chronological units is regarded a period, dates must be given as character string in the format \code("1000/2000"). The order does not matter. But also here, be consistent in order and in matching \code{start} and \code{end} dates to avoid unclean borders in the plot.
 #' The \code{level} indicates the position of the chronological unit. \code{level = 1} denotes a top chronological unit (e.g. Ha), a sub-unit (e.g. Ha B) is \code{level = 2}, a sub-sub-unit (e.g. Ha B1) \code{level = 3} etc. The parameter \code{add} indicates whether the respective chronological unit(s) should be plotted in the same or an additional column. This might be useful if competing chronologies in one region exist (e.g. short and long chronologies). See the vignette for a detailed explanation how the parameters \code{level} and \code{add} work.
-#' Additional columns useful for plotting might e,g, specify the x and y position of the names to place them at an arbitrary spot.
+#' Additional columns might be useful to e.g.  specify the x and y position of the names of the chronological units to place them at an arbitrary spot.
 #'
 #' @param data A data set with chronological data. Must not be provided if \code{new_table = FALSE}.
 #' @param region A character string or character vector with the title(s) of the section(s).
--- R/chronochrt_arrange_regions.R
+++ R/chronochrt_arrange_regions.R
@@ -2,9 +2,9 @@
 #'
 #' This function ensures that the regions/sections of a chronological chart are arranged different from an alphabetal order.
 #'
-#' @param data A data set with chronological data.
+#' @param data A data set with chronological data and a cloumn named "region".
 #' @param region A character string with the column name of the regions/sections
-#' @param order A character vector with right order of the region/section titles. Each title must be given only once.
+#' @param order A character vector with desired order of the region/section titles. Each title must be given only once.
 #'
 #' @return A tibble with chronological data ready-to-use for plotting with \code{\link[chronochrt]{plot_chronochrt}}.
 #'
@@ -12,29 +12,25 @@
 #'
 #' @examples
 
-arrange_regions <- function(data, region, order)
+arrange_regions <- function(data, order)
 {
   if (!exists(deparse(substitute(data)))) {
     stop("The object ", substitute(data) , " does not exist.")
-  }
-
-  if (!exists(deparse(substitute(order)))) {
-    stop("The object ", substitute(order) , " does not exist.")
   }
 
   if (!is.data.frame(data)) {
     stop("Wrong input format: ", substitute(data), " must be a data frame or tibble.")
   }
 
-  if (!is.character(region)) {
-    stop("Incompatible input format: ", substitute(region), " must be a character string.")
+  if (!"region" %in% names(data)) {
+    stop("Columns region does not exist in ", data, " .")
   }
 
   if (!is.character(order) && !is.vector(order)) {
     stop("Incompatible input format: ", substitute(order), " must be a vector of unique character strings.")
   }
 
-  data$region <- dplyr::mutate(data, !!region := factor(!!(as.name(region)), levels = order))
+  data$region <- factor(data$region, levels = order)
 
   data
 }
